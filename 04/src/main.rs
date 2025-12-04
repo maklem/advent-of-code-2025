8a2eea185bee0ed4c7aa18a0350e2d51a5dfeec7
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-12-04 09:11:52 +0100

    AoC 04 - Part 1

---
+++ 04/src/main.rs
@@ -0,0 +1,86 @@
+struct PaperRollStorage {
+    storage: Vec<Vec<bool>>,
+    rows: i32,
+    cols: i32,
+}
+
+impl PaperRollStorage {
+    pub fn new(input: String) -> Self {
+        let lines = input.lines();
+        let mut rows = 0;
+        let mut cols = 0;
+        let mut storage = Vec::<Vec::<bool>>::new();
+
+        for (index, line) in lines.enumerate() {
+            if cols == 0 {
+                cols = line.len();
+            }
+            if cols != line.len() {
+                panic!("non grid shape")
+            }
+            let mut linestorage = Vec::<bool>::new();
+            for entry in line.as_bytes() {
+                linestorage.push(match entry {
+                    b'.' => false,
+                    b'@' => true,
+                    _ => panic!("non storage entry in grid"),
+                });
+            }
+            storage.push(linestorage);
+            rows = index+1
+        }
+        PaperRollStorage { storage: storage, rows: rows as i32, cols: cols as i32 }
+    }
+    
+    pub fn is_paper_roll(&self, row: i32, col: i32) -> bool {
+        if (row < 0) || (col < 0) || (row >= self.rows) || (col >= self.cols) {
+            return false
+        }
+        self.storage[row as usize][col as usize]
+    }
+
+
+    pub fn count_neighbours(&self, row: i32, col: i32) -> i32 {
+        let mut count = 0;
+        for r in row-1..=row+1 {
+            for c in col-1..=col+1 {
+                if r == row && c == col {
+                    continue;
+                }
+                if self.is_paper_roll(r, c) {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+}
+
+fn main() {
+    let start = std::time::Instant::now();
+
+    let code = match std::fs::read_to_string("input.txt") {
+        Ok(file) => file,
+        Err(err) => panic!("Could not read file: {}", err),
+    };
+    let storage = PaperRollStorage::new(code);
+
+    let mut available_paper_rolls = 0;
+    for row in 0..storage.rows {
+        for col in 0..storage.cols {
+            if ! storage.is_paper_roll(row, col) {
+                continue;
+            }
+
+            if storage.count_neighbours(row, col) < 4 {
+                available_paper_rolls += 1;
+            }
+        }
+    }
+    println!("paper rolls available: {}", available_paper_rolls);
+
+    println!(
+        "evaluation took {} ms",
+        start.elapsed().as_nanos() as f64 / 1e6
+    )
+}
