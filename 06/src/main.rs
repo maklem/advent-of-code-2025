ca5b1e5a3efec1c54989b7e91be1c6e6aa2270d9
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-12-05 19:33:02 +0100

    AoC 06 - base program

---
+++ 06/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let start = std::time::Instant::now();
+
+    let code = match std::fs::read_to_string("input.txt") {
+        Ok(file) => file,
+        Err(err) => panic!("Could not read file: {}", err),
+    };
+
+    // ...
+
+    println!(
+        "evaluation took {} ms",
+        start.elapsed().as_nanos() as f64 / 1e6
+    )
+}
