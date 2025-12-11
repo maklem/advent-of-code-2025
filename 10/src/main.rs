67779d3bb1f6f01a488a9bd1ed3945213c654efa
Dr. Marco Klement <marco-klement-git@leuchtstark.dev>
2025-12-11 16:12:32 +0100

    AoC 10 - 02: prepare solving linear equations

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -1,4 +1,7 @@
-use std::{cmp::min, iter::zip};
+use std::{
+    cmp::{max, min},
+    iter::zip,
+};
 
 struct LightPattern {
     pattern: Vec<bool>,
@@ -54,25 +57,36 @@ impl Button {
     }
 }
 
+#[derive(Clone)]
 struct Joltages {
-    _values: Vec<usize>,
+    values: Vec<i64>,
 }
 
 impl Joltages {
     fn from_str(line: &str) -> Self {
         Self {
-            _values: line[1..line.len() - 1]
+            values: line[1..line.len() - 1]
                 .split(",")
                 .map(|s| s.parse().unwrap())
                 .collect(),
         }
+    }
+
+    fn apply_button(&mut self, button: &Button, count: usize) {
+        for index in &button.connected_lights {
+            self.values[*index] -= count as i64;
+        }
+    }
+
+    fn is_perfect(&self) -> bool {
+        self.values.iter().all(|v| *v == 0)
     }
 }
 
 struct Machine {
     pattern: LightPattern,
     buttons: Vec<Button>,
-    _joltages: Joltages,
+    joltages: Joltages,
 }
 
 fn to_machine(line: &str) -> Machine {
@@ -83,7 +97,44 @@ fn to_machine(line: &str) -> Machine {
             .iter()
             .map(|s| Button::from_str(s))
             .collect(),
-        _joltages: elem.last().map(|s| Joltages::from_str(s)).unwrap(),
+        joltages: elem.last().map(|s| Joltages::from_str(s)).unwrap(),
+    }
+}
+
+struct SystemOfLinearEquations {
+    rows: Vec<Vec<i64>>,
+}
+
+impl SystemOfLinearEquations {
+    fn from_machine(machine: &Machine) -> Self {
+        Self {
+            rows: machine
+                .joltages
+                .values
+                .iter()
+                .enumerate()
+                .map(|(index, jolts)| {
+                    let mut row: Vec<_> = machine
+                        .buttons
+                        .iter()
+                        .map(|btn| {
+                            if btn.connected_lights.contains(&index) {
+                                1
+                            } else {
+                                0
+                            }
+                        })
+                        .collect();
+                    row.push(*jolts);
+                    row
+                })
+                .collect(),
+        }
+    }
+
+    /// https://en.wikipedia.org/wiki/Gaussian_elimination
+    fn gaussian_elimination(&mut self, row: usize, column: usize) {
+
     }
 }
 
@@ -92,11 +143,16 @@ fn combinations(count: usize, slots: usize, max_count: usize) -> Vec<Vec<usize>>
     if count == 0 {
         result.push(vec![0; slots]);
     } else {
-        for padding in 0..=(slots - count) {
-            let mut local_pattern = vec![0; padding];
-            for current_count in 1..=min(count, max_count) {
+        let max_padding = slots - count.div_ceil(max_count);
+        for padding in 0..=max_padding {
+            let min_count = max(1, count - min(count, max_count * (slots - padding - 1)));
+            let max_count = min(count, max_count);
+            for current_count in min_count..=max_count {
+                let mut local_pattern = vec![0; padding];
                 local_pattern.push(current_count);
-                for combination in combinations(count - current_count, slots - padding - 1, max_count) {
+                for combination in
+                    combinations(count - current_count, slots - padding - 1, max_count)
+                {
                     let mut sub_pattern = local_pattern.clone();
                     combination.iter().for_each(|y| sub_pattern.push(*y));
                     result.push(sub_pattern);
@@ -104,10 +160,19 @@ fn combinations(count: usize, slots: usize, max_count: usize) -> Vec<Vec<usize>>
             }
         }
     }
+    if result.iter().any(|x| x.len() != slots) {
+        panic!(
+            "SIZE MISMATCH!! {} != {} (count: {})",
+            result.len(),
+            slots,
+            count
+        );
+    }
+
     result
 }
 
-fn find_minimal_button_presses_for_pattern(machine: Machine) -> usize {
+fn find_minimal_button_presses_for_pattern(machine: &Machine) -> usize {
     let button_count = machine.buttons.len();
     for available_buttons in 1..button_count {
         for combination in combinations(available_buttons, button_count, 1) {
@@ -128,6 +193,12 @@ fn find_minimal_button_presses_for_pattern(machine: Machine) -> usize {
     panic!("No suitable pattern found");
 }
 
+fn find_minimal_button_presses_for_joltage(machine: &Machine) -> usize {
+    let mut eqn = SystemOfLinearEquations::from_machine(machine);
+    eqn.gaussian_elimination();
+    0
+}
+
 fn main() {
     let start = std::time::Instant::now();
     let code = match std::fs::read_to_string("input.txt") {
@@ -137,10 +208,14 @@ fn main() {
     let machines: Vec<_> = code.lines().map(to_machine).collect();
 
     let mut button_presses = 0;
+    let mut joltage_presses = 0;
     for machine in machines {
-        button_presses += find_minimal_button_presses_for_pattern(machine);
+        button_presses += find_minimal_button_presses_for_pattern(&machine);
+        joltage_presses += find_minimal_button_presses_for_joltage(&machine);
+        print!(".")
     }
-    println!("Buttons pressed: {}", button_presses);
+    println!("[Part 1] Buttons pressed for pattern: {}", button_presses);
+    println!("[Part 2] Buttons pressed for joltage: {}", joltage_presses);
 
     println!(
         "evaluation took {} ms",
@@ -161,5 +236,15 @@ mod tests {
     #[test]
     fn combinations__for_entries_eq_slots__returns_list_of_true() {
         assert_eq!(vec![vec![1; 5]], combinations(5, 5, 1))
+    }
+
+    #[test]
+    fn combinations__for_entries_2x_slots__returns_list_of_two() {
+        assert_eq!(vec![vec![2; 5]], combinations(10, 5, 2))
+    }
+
+    #[test]
+    fn combinations__for_entries_5x_slots__returns_list_of_five() {
+        assert_eq!(vec![vec![5; 5]], combinations(25, 5, 5))
     }
 }
